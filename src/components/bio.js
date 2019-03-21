b2d27429c1a821d9c44b02d0ca0a11fcd4c28603
Kevin Wang <kwangsan@gmail.com>
2019-03-21 15:39:10 -0400

    update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,12 +38,8 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in New
-              York.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              A blog by <strong>{author}</strong>.{` `}
+              <a href={`https://twitter.com/${social.twitter}`}>Twitter</a>
             </p>
           </div>
         )
