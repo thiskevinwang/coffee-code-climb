2ad73d3ac772b091b2bccd14a727186234817e05
Kevin Wang <kwangsan@gmail.com>
2019-03-24 15:32:19 -0400

    temporarily disable `isLoggedIn` on PrivateRoute

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { navigate } from "gatsby"
-import { isLoggedIn } from "../services/auth"
+// import { isLoggedIn } from "../services/auth"
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-  if (!isLoggedIn() && location.pathname !== `/app/login`) {
-    // If the user is not logged in, redirect to the login page.
-    navigate(`/app/login`)
-    return null
-  }
+  // if (!isLoggedIn() && location.pathname !== `/app/login`) {
+  //   // If the user is not logged in, redirect to the login page.
+  //   navigate(`/app/login`)
+  //   return null
+  // }
 
   return <Component {...rest} />
 }
