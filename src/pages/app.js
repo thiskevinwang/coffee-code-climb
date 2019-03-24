2ea1b8a25611de9f1f56e37a6cbf3be7fc73d515
Kevin Wang <kwangsan@gmail.com>
2019-03-24 15:31:56 -0400

    remove login page from app.js

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,13 +3,13 @@ import { Router } from "@reach/router"
 import Layout from "../components/layout"
 import PrivateRoute from "../components/privateRoute"
 import Profile from "../components/profile"
-import Login from "../components/login"
+// import Login from "../components/login"
 
 const App = props => (
   <Layout location={props.location}>
     <Router>
       <PrivateRoute path="/app/profile" component={Profile} />
-      <Login path="/app/login" />
+      {/* <Login path="/app/login" /> */}
     </Router>
   </Layout>
 )
