f0cd384e0279fc4578c461743f500a3bb94ffbc4
Kevin Wang <kwangsan@gmail.com>
2019-03-24 00:25:07 -0400

    create app.js page - generic page that will have the task to generate the restricted content:

---
+++ src/pages/app.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Router } from "@reach/router"
+import Layout from "../components/layout"
+import PrivateRoute from "../components/privateRoute"
+import Profile from "../components/profile"
+import Login from "../components/login"
+
+const App = props => (
+  <Layout location={props.location}>
+    <Router>
+      <PrivateRoute path="/app/profile" component={Profile} />
+      <Login path="/app/login" />
+    </Router>
+  </Layout>
+)
+
+export default App
