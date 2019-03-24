086d24b5b55f13e60a1bf627be759d92e2a4d1de
Kevin Wang <kwangsan@gmail.com>
2019-03-24 15:31:35 -0400

    Update navbar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,26 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link, navigate } from "gatsby"
-import { getUser, isLoggedIn, logout } from "../services/auth"
+// import { getUser, isLoggedIn, logout } from "../services/auth"
+import auth from "../utils/auth"
 import Paper from "@material-ui/core/Paper"
-import { isSafari, isFirefox } from "react-device-detect"
+import { isSafari, isFirefox, isMobile } from "react-device-detect"
+import { rhythm, scale } from "../utils/typography"
+import Avatar from "@material-ui/core/Avatar"
 
-export default () => {
-  const content = { message: "", login: true }
-  if (isLoggedIn()) {
-    content.message = `Hello, ${getUser().name}`
-  } else {
-    content.message = "You are not logged in"
+export default function NavBar({ location }) {
+  const rootPath: string = `${__PATH_PREFIX__}/`
+  const [authenticated, setAuthenticated] = useState(auth.isAuthenticated())
+
+  function login() {
+    auth.login()
+    setAuthenticated(auth.isAuthenticated())
   }
+
+  function logout() {
+    auth.logout()
+    setAuthenticated(auth.isAuthenticated())
+  }
+
   return (
     <Paper
       style={{
         background: "white",
-        position: isSafari
+        position: isMobile
+          ? `sticky`
+          : isSafari
           ? `-webkit-sticky`
           : isFirefox
           ? `-moz-sticky`
           : `sticky`,
-        top: 15,
+        top: `${rhythm(1)}`,
         left: 0,
         display: "flex",
         flex: "1",
@@ -28,19 +40,53 @@ export default () => {
         alignItems: "center",
         borderBottom: "1px solid #d1c1e0",
         height: 70,
-        paddingLeft: 15,
-        paddingRight: 15,
-        marginBottom: 15,
+        padding: `0 ${rhythm(3 / 4)}`,
+        marginBottom: `${rhythm(1)}`,
         zIndex: 9999,
       }}
     >
-      <span>{content.message}</span>
-      <nav>
-        <Link to="/">Home</Link>
-        {` `}
-        <Link to="/app/profile">Profile</Link>
-        {` `}
-        {isLoggedIn() ? (
+      {authenticated ? (
+        <>
+          <Avatar
+            sizes={"large"}
+            alt={auth.getUserName()}
+            src={auth.getUser().picture}
+          />
+          {auth.getUserName() && <small>{auth.getUserName()}</small>}
+        </>
+      ) : (
+        <small>{location.pathname != rootPath ? "😊" : "👋"}</small>
+      )}
+      <small>
+        {location.pathname != rootPath && (
+          <>
+            <Link
+              to="/"
+              style={{
+                boxShadow: "none",
+                lineHeight: "37px",
+              }}
+            >
+              Home
+            </Link>
+            {" | "}
+          </>
+        )}
+        {authenticated && location.pathname != "/app/profile" && (
+          <>
+            <Link
+              to="/app/profile"
+              style={{
+                boxShadow: "none",
+                lineHeight: "37px",
+              }}
+            >
+              Profile
+            </Link>{" "}
+            |{" "}
+          </>
+        )}
+        {/* {isLoggedIn() ? (
           <a
             href="/"
             onClick={event => {
@@ -50,8 +96,38 @@ export default () => {
           >
             Logout
           </a>
-        ) : null}
-      </nav>
+        ) : null} */}
+        {!authenticated && (
+          <span>
+            <a
+              href="#"
+              onClick={login}
+              style={{
+                boxShadow: "none",
+                lineHeight: "37px",
+              }}
+            >
+              Log In
+            </a>
+          </span>
+        )}
+        {authenticated && (
+          <span>
+            <a
+              href="#"
+              onClick={logout}
+              style={{
+                boxShadow: "none",
+                lineHeight: "37px",
+              }}
+            >
+              Log Out
+            </a>
+            {/* <span> | </span> */}
+            {/* <Subscribe auth={auth} /> */}
+          </span>
+        )}
+      </small>
     </Paper>
   )
 }
