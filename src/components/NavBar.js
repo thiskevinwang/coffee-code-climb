b3d4a8b87a179ab95ae5ae8ba9728bdf420b7bbe
Kevin Wang <kwangsan@gmail.com>
2019-03-26 20:50:11 -0400

    fix console warnings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react"
-import { Link, navigate } from "gatsby"
+import React, { useState } from "react"
+import { Link } from "gatsby"
 // import { getUser, isLoggedIn, logout } from "../services/auth"
 // import auth from "../utils/auth"
 import Paper from "@material-ui/core/Paper"
 import { isSafari, isFirefox, isMobile } from "react-device-detect"
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography"
 import Avatar from "@material-ui/core/Avatar"
 
 export default function NavBar({ location }) {
@@ -51,11 +51,11 @@ export default function NavBar({ location }) {
         </>
       ) : (
         <small>
-          {location.pathname != rootPath ? "😊" : "NavBar, coming soon! 👷‍♀️🚛"}
+          {location.pathname !== rootPath ? "😊" : "NavBar, coming soon! 👷‍♀️🚛"}
         </small>
       )}
       <small>
-        {location.pathname != rootPath && (
+        {location.pathname !== rootPath && (
           <>
             <Link
               to="/"
@@ -69,7 +69,7 @@ export default function NavBar({ location }) {
             {" | "}
           </>
         )}
-        {authenticated && location.pathname != "/app/profile" && (
+        {authenticated && location.pathname !== "/app/profile" && (
           <>
             <Link
               to="/app/profile"
