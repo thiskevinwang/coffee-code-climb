fe9099e81b114a8a8a87d7be0546728cf7a3246c
Kevin Wang <kwangsan@gmail.com>
2019-04-21 17:01:58 -0400

    create contentful-blog-post template

---
+++ src/templates/contentful-blog-post.tsx
@@ -0,0 +1,174 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import kebabCase from "lodash/kebabCase"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
+import Image from "gatsby-image"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm, scale } from "@src/utils/typography"
+import { DiscussionEmbed } from "disqus-react"
+
+export default function ContentfulBlogPostTemplate(props) {
+  const post = props.data.contentfulBlogPost
+  const siteTitle = props.data.site.siteMetadata.title
+  const { previous, next } = props.pageContext
+
+  /**
+   * Arguments to pass to: documentToReactComponents(document, options)
+   * https://github.com/contentful/rich-text/tree/master/packages/rich-text-react-renderer
+   **/
+  const document: JSON = post.body.json
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        // console.log(node)
+        let { file, title, description } = node.data.target.fields
+        // console.log(file["en-US"].url)
+        return <img src={file["en-US"].url} alt={description["en-US"]} />
+      },
+    },
+  }
+  // console.log(documentToReactComponents(document, options))
+
+  const disqusShortname = "coffeecodeclimb"
+  const disqusConfig = {
+    url: "https://coffeecodeclimb.com" + props.location.pathname,
+    identifier: post.id,
+    title: post.title,
+  }
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO title={post.title} description={post.description} />
+      <h1>{post.title}</h1>
+
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1),
+        }}
+      >
+        {post.date}
+      </p>
+
+      {/**
+       * NOTE: replace this div with documentToReactComponents()
+       * It converts Contenful's "rich text" (post.body.json)
+       * to react components.
+       *
+       * <div dangerouslySetInnerHTML={{ __html: post.html }} />
+       **/}
+      {documentToReactComponents(document, options)}
+
+      <small>
+        Tags:{" "}
+        {post.tags.map((tag, index) => (
+          <Link
+            to={`/tags/${kebabCase(tag)}/`}
+            style={{ color: "#A6B1BB", margin: 3 }}
+            key={index}
+          >
+            {tag}
+          </Link>
+        ))}
+      </small>
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio />
+
+      <ul
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        }}
+      >
+        {/**
+         * [previous|next].internal.type = `MarkdownRemark` || `ContentfulBlogPost`
+         * slug & title need to be accessed slightly differently
+         **/}
+        <li>
+          {previous && (
+            <Link
+              to={
+                previous.internal.type === `MarkdownRemark`
+                  ? previous.fields.slug
+                  : previous.slug
+              }
+              rel="prev"
+            >
+              ←{" "}
+              {previous.internal.type === `MarkdownRemark`
+                ? previous.frontmatter.title
+                : previous.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link
+              to={
+                next.internal.type === `MarkdownRemark`
+                  ? next.fields.slug
+                  : next.slug
+              }
+              rel="next"
+            >
+              {next.internal.type === `MarkdownRemark`
+                ? next.frontmatter.title
+                : next.title}{" "}
+              →
+            </Link>
+          )}
+        </li>
+      </ul>
+
+      <h2>Discussion</h2>
+      <div className="disqus">
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query ContentfulBlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    # markdownRemark(fields: { slug: { eq: $slug } }) {
+    #   id
+    #   excerpt(pruneLength: 160)
+    #   html
+    #   frontmatter {
+    #     title
+    #     date(formatString: "MMMM DD, YYYY")
+    #     description
+    #     tags
+    #   }
+    # }
+    contentfulBlogPost(slug: { eq: $slug }) {
+      id
+      body {
+        json
+      }
+      title
+      date(formatString: "MMMM DD, YYYY")
+      description
+      tags
+    }
+  }
+`
