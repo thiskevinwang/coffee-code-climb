4f91a5c7fa4a53963d72d26b8d028021ba25a14d
Kevin Wang <kwangsan@gmail.com>
2019-08-06 08:04:00 -0400

    singlular `useSpring`; memo() <Post>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react"
+import React, { useState, memo } from "react"
+import { useSelector } from "react-redux"
 import _ from "lodash"
 import moment from "moment"
 import { Link, graphql } from "gatsby"
 import Image from "gatsby-image"
 import { CommentCount } from "disqus-react"
 import { Grid, Tooltip } from "@material-ui/core"
-import { animated, useSprings, AnimatedValue, config } from "react-spring"
+import { animated, useSpring, AnimatedValue, config } from "react-spring"
 import styled from "styled-components"
 
 import Bio from "../components/bio"
@@ -37,6 +38,31 @@ const KEYWORDS = [
   `node`,
   `nextjs`,
 ]
+
+/**
+ * # FROM_STYLE
+ * - starting animated-style
+ */
+const FROM_STYLE = {
+  boxShadow: `0px 15px 30px -15px ${Colors.blackDark}`,
+  transform: `scale(1)`,
+}
+/**
+ * # MOUSEOVER_STYLE
+ * - target animated-style
+ */
+const MOUSEOVER_STYLE = {
+  boxShadow: `0px 17px 40px -13px ${Colors.blackDarker}`,
+  transform: `scale(1.01)`,
+}
+/**
+ * # MOUSEDOWN_STYLE
+ */
+const MOUSEDOWN_STYLE = {
+  boxShadow: `0px 15px 20px -17px ${Colors.blackDarker}`,
+  transform: `scale(0.98)`,
+}
+
 interface Props {
   key: string
   linkTo: string
@@ -50,149 +76,156 @@ interface Props {
   index: number
   nodeType: string
   animatedStyles: AnimatedValue<any>
-  _handleMouseEnter: () => void
-  _handleMouseLeave: () => void
-  _handleMouseDown: () => void
-  _handleMouseUp: () => void
-  _handleTouchStart: () => void
-  _handleTouchEnd: () => void
 }
 
-function Post({
-  linkTo,
-  date,
-  title,
-  description,
-  excerpt,
-  tags,
-  // origin,
-  id,
-  image,
-  index,
-  nodeType,
-  animatedStyles,
-  _handleMouseEnter,
-  _handleMouseLeave,
-  _handleMouseDown,
-  _handleMouseUp,
-  _handleTouchStart,
-  _handleTouchEnd,
-}: Props) {
-  //_.map + _.kebabCase each tag in frontmatter.tags
-  let kebabTags = _.map(tags, tag => _.kebabCase(tag))
+// To avoid <Post> rerenders when <BlogPostIndex> subscribes to redux
+// state(like isDarkMode), add `memo()` here
+const Post = memo(
+  ({
+    linkTo,
+    date,
+    title,
+    description,
+    excerpt,
+    tags, // origin,
+    id,
+    image,
+    index,
+    nodeType,
+  }: Props) => {
+    //_.map + _.kebabCase each tag in frontmatter.tags
+    let kebabTags = _.map(tags, tag => _.kebabCase(tag))
 
-  const disqusShortname = "coffeecodeclimb"
-  const disqusConfig = {
-    url: "https://coffeecodeclimb.com" + linkTo,
-    identifier: id,
-    title: title,
-  }
+    const disqusShortname = "coffeecodeclimb"
+    const disqusConfig = {
+      url: "https://coffeecodeclimb.com" + linkTo,
+      identifier: id,
+      title: title,
+    }
 
-  const Card = animated(styled.div`
+    const Card = animated(styled.div`
     border-radius: 5px;
     /* box-shadow: 0px 10px 40px -10px ${Colors.blackDark}; */
     /* This clips the square top corners of the child image */
     overflow: hidden;
   `)
 
-  const PostDetails = (
-    <div
-      style={{
-        margin: rhythm(2 / 3),
-      }}
-    >
-      <Link style={{ color: `inherit`, boxShadow: `none` }} to={linkTo}>
-        <h3 style={{ marginTop: rhythm(1 / 2), marginBottom: rhythm(1 / 2) }}>
-          {title}
-        </h3>
-        <small>{date}</small>
-        <br />
-        <small
-          dangerouslySetInnerHTML={{
-            __html: description || excerpt,
-          }}
-        />
-      </Link>
-      <br />
-      {_.includes(kebabTags, "coffee") && (
-        <Tooltip title={`tagged with "coffee"`}>
-          <Link style={{ boxShadow: `none` }} to={"/tags/coffee/"}>
-            <span role="img" aria-label="tagged with coffee">
-              ☕️
-            </span>
-          </Link>
-        </Tooltip>
-      )}
-      {_.includes(kebabTags, "code") && (
-        <Tooltip title={`tagged with "code"`}>
-          <Link style={{ boxShadow: `none` }} to={"/tags/code/"}>
-            <span role="img" aria-label="tagged with code">
-              💻
-            </span>
-          </Link>
-        </Tooltip>
-      )}
-      {_.includes(kebabTags, "climbing") && (
-        <Tooltip title={`tagged with "climbing"`}>
-          <Link style={{ boxShadow: `none` }} to={"/tags/climbing/"}>
-            <span role="img" aria-label="tagged with climbing">
-              🧗🏻‍♂️
-            </span>
-          </Link>
-        </Tooltip>
-      )}{" "}
-      <code>
-        <small>
-          <Link
-            style={{ color: `inherit`, boxShadow: `none` }}
-            to={`${linkTo}#disqus_thread`}
-          >
-            <CommentCount shortname={disqusShortname} config={disqusConfig}>
-              Comments
-            </CommentCount>
-          </Link>
-        </small>
-      </code>
-    </div>
-  )
-
-  return (
-    <Grid
-      item
-      lg={index === 0 ? 12 : 3}
-      md={index === 0 ? 12 : 4}
-      sm={index === 0 ? 12 : 6}
-      xs={12}
-    >
-      <Card
-        className={"Card"}
-        style={animatedStyles}
-        onMouseEnter={_handleMouseEnter}
-        onMouseLeave={_handleMouseLeave}
-        onMouseDown={_handleMouseDown}
-        onMouseUp={_handleMouseUp}
-        onTouchStart={_handleTouchStart}
-        onTouchEnd={_handleTouchEnd}
+    const PostDetails = (
+      <div
+        style={{
+          margin: rhythm(2 / 3),
+        }}
       >
-        <Link style={{ boxShadow: `none` }} to={linkTo}>
-          {image && (
-            <Image
-              fluid={
-                nodeType === `MarkdownRemark`
-                  ? image.childImageSharp.fluid
-                  : image.fluid
-              }
-              alt={linkTo}
-              style={{}}
-              imgStyle={{}}
-            />
-          )}
+        <Link style={{ color: `inherit`, boxShadow: `none` }} to={linkTo}>
+          <h3 style={{ marginTop: rhythm(1 / 2), marginBottom: rhythm(1 / 2) }}>
+            {title}
+          </h3>
+          <small>{date}</small>
+          <br />
+          <small
+            dangerouslySetInnerHTML={{
+              __html: description || excerpt,
+            }}
+          />
         </Link>
-        {PostDetails}
-      </Card>
-    </Grid>
-  )
-}
+        <br />
+        {_.includes(kebabTags, "coffee") && (
+          <Tooltip title={`tagged with "coffee"`}>
+            <Link style={{ boxShadow: `none` }} to={"/tags/coffee/"}>
+              <span role="img" aria-label="tagged with coffee">
+                ☕️
+              </span>
+            </Link>
+          </Tooltip>
+        )}
+        {_.includes(kebabTags, "code") && (
+          <Tooltip title={`tagged with "code"`}>
+            <Link style={{ boxShadow: `none` }} to={"/tags/code/"}>
+              <span role="img" aria-label="tagged with code">
+                💻
+              </span>
+            </Link>
+          </Tooltip>
+        )}
+        {_.includes(kebabTags, "climbing") && (
+          <Tooltip title={`tagged with "climbing"`}>
+            <Link style={{ boxShadow: `none` }} to={"/tags/climbing/"}>
+              <span role="img" aria-label="tagged with climbing">
+                🧗🏻‍♂️
+              </span>
+            </Link>
+          </Tooltip>
+        )}{" "}
+        <code>
+          <small>
+            <Link
+              style={{ color: `inherit`, boxShadow: `none` }}
+              to={`${linkTo}#disqus_thread`}
+            >
+              <CommentCount shortname={disqusShortname} config={disqusConfig}>
+                Comments
+              </CommentCount>
+            </Link>
+          </small>
+        </code>
+      </div>
+    )
+
+    const [springProps, set] = useSpring(() => {
+      return {
+        from: { ...FROM_STYLE },
+        to: { ...FROM_STYLE },
+        config: config.wobbly,
+      }
+    })
+
+    /**
+     * updateStyles
+     * - logging `idx` logs all the indices
+     * - if the hovered index ===
+     */
+    const updateStyles = stylesObject => e => {
+      set({ ...stylesObject })
+    }
+
+    return (
+      <Grid
+        item
+        lg={index === 0 ? 12 : 3}
+        md={index === 0 ? 12 : 4}
+        sm={index === 0 ? 12 : 6}
+        xs={12}
+      >
+        <Card
+          className={"Card"}
+          style={springProps}
+          onMouseEnter={updateStyles(MOUSEOVER_STYLE)}
+          onMouseLeave={updateStyles(FROM_STYLE)}
+          onMouseDown={updateStyles(MOUSEDOWN_STYLE)}
+          onMouseUp={updateStyles(MOUSEOVER_STYLE)}
+          onTouchStart={updateStyles(MOUSEDOWN_STYLE)}
+          onTouchEnd={updateStyles(FROM_STYLE)}
+        >
+          <Link style={{ boxShadow: `none` }} to={linkTo}>
+            {image && (
+              <Image
+                fluid={
+                  nodeType === `MarkdownRemark`
+                    ? image.childImageSharp.fluid
+                    : image.fluid
+                }
+                alt={linkTo}
+                style={{}}
+                imgStyle={{}}
+              />
+            )}
+          </Link>
+          {PostDetails}
+        </Card>
+      </Grid>
+    )
+  }
+)
 
 /**
  * BlogIndex
@@ -202,6 +235,9 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const markdownPosts = data.allMarkdownRemark.edges
   const contentfulPosts = data.allContentfulBlogPost.edges
+
+  // use this to test child rerenders
+  const isDarkMode = useSelector(state => state.isDarkMode)
 
   /**
    * Combine Markdown & Contentful posts. Sort by newest Date.
@@ -218,62 +254,22 @@ const BlogIndex = ({ data, location }) => {
     }
   )
 
-  /**
-   * # FROM_STYLE
-   * - starting animated-style
-   */
-  const FROM_STYLE = {
-    boxShadow: `0px 15px 30px -15px ${Colors.blackDark}`,
-    transform: `scale(1)`,
-  }
-  /**
-   * # MOUSEOVER_STYLE
-   * - target animated-style
-   */
-  const MOUSEOVER_STYLE = {
-    boxShadow: `0px 17px 40px -13px ${Colors.blackDarker}`,
-    transform: `scale(1.01)`,
-  }
-  /**
-   * # MOUSEDOWN_STYLE
-   */
-  const MOUSEDOWN_STYLE = {
-    boxShadow: `0px 15px 20px -17px ${Colors.blackDarker}`,
-    transform: `scale(0.98)`,
-  }
-
-  const [springs, set] = useSprings(posts.length, index => {
-    return {
-      from: { ...FROM_STYLE },
-      to: { ...FROM_STYLE },
-      config: config.wobbly,
-    }
-  })
-
-  /**
-   * updateStyles
-   * - logging `idx` logs all the indices
-   * - if the hovered index ===
-   */
-  const updateStyles = (index, stylesObject) => e => {
-    set(idx => {
-      return idx === index && { ...stylesObject }
-    })
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" keywords={KEYWORDS} />
       <Bio />
 
       <Grid container direction="row" spacing={4}>
-        {springs.map((props, index, propsArray) => {
-          const { node } = posts[index]
+        {posts.map(({ node }, index) => {
+          const isMarkdownRemark = node.internal.type === `MarkdownRemark`
+          const isContentfulBlogPost =
+            node.internal.type === `ContentfulBlogPost`
+
           const title =
             node.internal.type === `MarkdownRemark` &&
             (node.frontmatter.title || node.fields.slug)
 
-          return node.internal.type === `MarkdownRemark` ? (
+          return isMarkdownRemark ? (
             <Post
               key={node.fields.slug}
               linkTo={node.fields.slug}
@@ -287,13 +283,6 @@ const BlogIndex = ({ data, location }) => {
               image={node.frontmatter.image}
               index={index}
               nodeType={node.internal.type}
-              animatedStyles={{ ...props }}
-              _handleMouseEnter={updateStyles(index, MOUSEOVER_STYLE)}
-              _handleMouseLeave={updateStyles(index, FROM_STYLE)}
-              _handleMouseDown={updateStyles(index, MOUSEDOWN_STYLE)}
-              _handleMouseUp={updateStyles(index, MOUSEOVER_STYLE)}
-              _handleTouchStart={updateStyles(index, MOUSEDOWN_STYLE)}
-              _handleTouchEnd={updateStyles(index, MOUSEOVER_STYLE)}
             />
           ) : (
             /**
@@ -312,13 +301,6 @@ const BlogIndex = ({ data, location }) => {
               image={node.image}
               index={index}
               nodeType={node.internal.type}
-              animatedStyles={{ ...props }}
-              _handleMouseEnter={updateStyles(index, MOUSEOVER_STYLE)}
-              _handleMouseLeave={updateStyles(index, FROM_STYLE)}
-              _handleMouseDown={updateStyles(index, MOUSEDOWN_STYLE)}
-              _handleMouseUp={updateStyles(index, MOUSEOVER_STYLE)}
-              _handleTouchStart={updateStyles(index, MOUSEDOWN_STYLE)}
-              _handleTouchEnd={updateStyles(index, MOUSEOVER_STYLE)}
             />
           )
         })}
