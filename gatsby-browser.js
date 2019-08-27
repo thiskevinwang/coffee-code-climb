5001a2f88c88d11ff59816c6ed6fa2a28c8819f7
Kevin Wang <kwangsan@gmail.com>
2019-08-26 22:16:14 -0400

    add IO check in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,3 +11,14 @@ import { store } from "./src/state"
 export const wrapRootElement = ({ element }) => {
   return <Provider store={store}>{element}</Provider>
 }
+
+/**
+ * Fix Error:
+ * `WebpackError: ReferenceError: IntersectionObserver is not defined`
+ * https://github.com/gatsbyjs/gatsby/issues/10435#issuecomment-446627549
+ */
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`)
+  }
+}
