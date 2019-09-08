15bd70b2b66af5db673d811d6837739eba3c7a65
Kevin Wang <kwangsan@gmail.com>
2019-09-08 08:35:08 -0400

    add SpringButton & toggle blog image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,52 @@
 import React from "react"
+import { useSelector, useDispatch } from "react-redux"
 import _ from "lodash"
 import moment from "moment"
 import { graphql } from "gatsby"
 import { Grid } from "@material-ui/core"
+import { animated, useSpring, config } from "react-spring"
+import styled from "styled-components"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Post } from "../components/Post"
+import { KEYWORDS } from "../consts"
+import { setShowBlogImage } from "../state"
 
-const KEYWORDS = [
-  `blog`,
-  `gatsby`,
-  `gatsbyjs`,
-  `react`,
-  `reactjs`,
-  `new york`,
-  `rock climbing`,
-  `javascript`,
-  `react`,
-  `coffee`,
-  `coding`,
-  `climb`,
-  `coffeecodeclimb`,
-  `coffee code climb`,
-  `climbing`,
-  `kevin wang`,
-  `graphql`,
-  `apollo`,
-  `node`,
-  `nextjs`,
-  `redux`,
-  `hooks`,
-  `typescript`,
-]
+import { rhythm } from "src/utils/typography"
+import * as Colors from "consts/Colors"
 
+/**
+ * # FROM_STYLE
+ * - starting animated-style
+ */
+const FROM_STYLE = {
+  boxShadow: `0px 10px 20px -10px ${Colors.blackDark}`,
+  transform: `scale(1)`,
+}
+/**
+ * # MOUSEOVER_STYLE
+ * - target animated-style
+ */
+const MOUSEOVER_STYLE = {
+  boxShadow: `0px 12px 25px -8px ${Colors.blackDarker}`,
+  transform: `scale(1.05)`,
+}
+/**
+ * # MOUSEDOWN_STYLE
+ */
+const MOUSEDOWN_STYLE = {
+  boxShadow: `0px 10px 10px -12px ${Colors.blackDarker}`,
+  transform: `scale(0.98)`,
+}
+const SpringButton = styled(animated.button)`
+  border-radius: 5px;
+  border: none;
+  letter-spacing: ${rhythm(0.1)};
+  margin-bottom: ${rhythm(1)};
+  text-transform: uppercase;
+`
 /**
  * BlogIndex
  * Our landing page component!
@@ -43,6 +55,22 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const markdownPosts = data.allMarkdownRemark.edges
   const contentfulPosts = data.allContentfulBlogPost.edges
+
+  const showBlogImage = useSelector(state => state.showBlogImage)
+  const dispatch = useDispatch()
+  const dispatchToggleBlogImage = () =>
+    dispatch(setShowBlogImage(!showBlogImage))
+
+  const [springProps, set] = useSpring(() => {
+    return {
+      from: { ...FROM_STYLE },
+      to: { ...FROM_STYLE },
+      config: config.wobbly,
+    }
+  })
+  const updateStyles = stylesObject => e => {
+    set({ ...stylesObject })
+  }
 
   /**
    * Combine Markdown & Contentful posts. Sort by newest Date.
@@ -63,6 +91,19 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" keywords={KEYWORDS} />
       <Bio />
+
+      <SpringButton
+        style={springProps}
+        onMouseEnter={updateStyles(MOUSEOVER_STYLE)}
+        onMouseLeave={updateStyles(FROM_STYLE)}
+        onMouseDown={updateStyles(MOUSEDOWN_STYLE)}
+        onMouseUp={updateStyles(MOUSEOVER_STYLE)}
+        onTouchStart={updateStyles(MOUSEDOWN_STYLE)}
+        onTouchEnd={updateStyles(FROM_STYLE)}
+        onClick={dispatchToggleBlogImage}
+      >
+        {showBlogImage ? "hide images" : "show images"}
+      </SpringButton>
 
       <Grid container direction="row" spacing={4}>
         {posts.map(({ node }, index) => {
@@ -92,6 +133,7 @@ const BlogIndex = ({ data, location }) => {
               image={isMarkdownRemark ? node.frontmatter.image : node.image}
               index={index}
               nodeType={node.internal.type}
+              showBlogImage={showBlogImage}
             />
           )
         })}
