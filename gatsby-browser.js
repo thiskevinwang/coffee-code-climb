80ba3b39885f871628369a3da97a7a9337bab3b8
Kevin Wang <kwangsan@gmail.com>
2019-09-22 03:21:27 -0400

    do some apollo stuff in browser `onRouteUpdate`

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,7 @@ import * as React from "react"
 import { Provider } from "react-redux"
 
 import { store } from "./src/state"
+import { client, CREATE_PAGE, GET_PAGE, INCREMENT_VIEWS } from "src/apollo"
 
 // export const wrapRootElement vs. exports.wrapRootElement...
 export const wrapRootElement = ({ element }) => {
@@ -21,4 +22,51 @@ export const onClientEntry = async () => {
   if (typeof IntersectionObserver === `undefined`) {
     await import(`intersection-observer`)
   }
+}
+
+// fires when arriving and leaving...
+export const onRouteUpdate = ({ location, prevLocation, ...rest }, plugins) => {
+  // console.log({ location })
+  try {
+    client
+      .mutate({
+        mutation: CREATE_PAGE,
+        variables: {
+          id: 1,
+          location: location.href,
+        },
+      })
+      .then(result => {
+        console.log("CREATE_PAGE", result)
+      })
+  } catch (err) {
+    console.log("I think this page was already created:", err)
+  }
+
+  // client
+  //   .query({
+  //     query: GET_PAGE,
+  //     variables: {
+  //       id: 1,
+  //       location: location.href,
+  //     },
+  //   })
+  //   .then(result => {
+  //     console.log("GET_PAGE", result)
+  //   })
+  client
+    .mutate({
+      mutation: INCREMENT_VIEWS,
+      variables: {
+        id: 1,
+        location: location.href,
+      },
+    })
+    .then(result => {
+      console.log(
+        "INCREMENT_VIEWS",
+        location.href,
+        result.data.incrementViews.views
+      )
+    })
 }
