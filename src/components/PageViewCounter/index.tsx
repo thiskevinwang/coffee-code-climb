bcce83fb7cf8bad17d74fdd3d7a2f241d61bffe9
Kevin Wang <kwangsan@gmail.com>
2019-09-22 14:25:22 -0400

    PageViewCounter improvements!!!

--- src/components/PageViewCounter/index.tsx
+++ src/components/PageViewCounter/index.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect } from "react"
-import { useLazyQuery } from "@apollo/react-hooks"
+import { useLazyQuery, useApolloClient } from "@apollo/react-hooks"
+import styled from "styled-components"
 
 import { GET_PAGE } from "apollo"
+
+const Small = styled.small`
+  background: none;
+  position: fixed;
+  top: 70px;
+  padding-left: 5px;
+`
 
 /**
  * PageViewCounter
@@ -14,25 +22,39 @@ const PageViewCounter = ({ location }: { location: Location }) => {
   const [getPageViews, { called, loading, data, error }] = useLazyQuery(
     GET_PAGE
   )
+  const client = useApolloClient()
+
   useEffect(() => {
-    console.log("fetching")
-    getPageViews({
-      variables: { id: 1, location: location.href },
-    })
-    return () => {}
+    /**
+     * @TODO find an alternative to `setTimeout` to deal with race vs. Gatsby.onRouteUpdate()
+     */
+    setTimeout(
+      () =>
+        getPageViews({
+          variables: { id: 1, location: location.href },
+        }),
+      500
+    )
+
+    return () => {
+      /**
+       * this fixes an issue with page view being cached when going 'back'
+       */
+      client.resetStore()
+    }
   }, [location.href])
 
-  if (!called) return <small>not called</small>
-  if (called && loading) return <small>...</small>
-  if (error) return <small>oops</small>
+  if (!called) return <Small>.</Small>
+  if (called && loading) return <Small>...</Small>
+  if (error) return <Small>oops</Small>
 
-  if (!data.getPage) return <small>oops</small>
-  if (!data.getPage.attributes) return <small>oops</small>
+  if (!data.getPage) return <Small>oops</Small>
+  if (!data.getPage.attributes) return <Small>oops</Small>
 
   return (
-    <small style={{ position: "sticky", top: 70, paddingLeft: 5 }}>{`${
-      data.getPage.attributes.views
-    } ${data.getPage.attributes.views === 1 ? "view" : "views"}`}</small>
+    <Small>{`${data.getPage.attributes.views} ${
+      data.getPage.attributes.views === 1 ? "view" : "views"
+    }`}</Small>
   )
 }
 
