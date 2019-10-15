094241e518c4ccda6f4a5b3a283f3a3fbcb1a054
Kevin Wang <kwangsan@gmail.com>
2019-10-14 21:24:47 -0400

    add TableOfContents component

---
+++ src/components/TableOfContents.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react"
+import styled from "styled-components"
+import { useSpring, animated } from "react-spring"
+import { useScroll } from "react-use-gesture"
+import { Link } from "gatsby"
+
+const DesktopContainer = styled(animated.div)`
+  position: absolute;
+  right: 0;
+  margin-right: 20px;
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
+
+  li {
+    list-style: none;
+  }
+  * {
+    font-size: 12px;
+  }
+`
+
+const TableOfContents = ({ title, __html }) => {
+  const [{ top }, set] = useSpring(() => ({ top: 50 }))
+
+  const bindScrollGesture = useScroll(
+    state => {
+      const { scrollTop } = state.event.target.documentElement
+
+      // console.log(scrollTop / (scrollHeight - clientHeight))
+      set({
+        top: scrollTop + 50,
+      })
+    },
+    { domTarget: typeof window !== "undefined" && window }
+  )
+  useEffect(bindScrollGesture, [bindScrollGesture])
+
+  return (
+    <DesktopContainer style={{ top }}>
+      <div style={{ textAlign: "center", marginBottom: 20, fontSize: 20 }}>
+        Table of Contents
+      </div>
+      <Link to={window.location.pathname}>{title}</Link>
+      <div dangerouslySetInnerHTML={{ __html }} />
+    </DesktopContainer>
+  )
+}
+
+export { TableOfContents }
