b50ec2bc9ea27a5de489badbc4e0f8ef0fcb5134
Kevin Wang <kwangsan@gmail.com>
2019-10-25 23:17:46 -0400

    basic enter transition

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 import * as React from "react"
-import { Provider } from "react-redux"
+import { Provider, useSelector, useDispatch } from "react-redux"
+import { Transition } from "react-transition-group"
+import { useSpring, animated } from "react-spring"
 
 import { store } from "./src/_reduxState"
 import {
@@ -10,21 +12,87 @@ import {
   INCREMENT_VIEWS,
 } from "src/apollo"
 
+const duration = 600
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+}
+
+const RootTransitionWrapper = props => {
+  const [inProp, setInProp] = React.useState(false)
+
+  /**
+   * @TODO Spring animate this
+   */
+  // const [{ opacity }, set] = useSpring(() => ({
+  //   opacity: inProp ? 1 : 0,
+  // }))
+
+  React.useEffect(() => {
+    setInProp(true)
+    return () => {}
+  }, [])
+
+  return (
+    <Transition timeout={duration} in={inProp} appear={true}>
+      {state => {
+        return (
+          <animated.div
+            // style={{ opacity }}
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            {props.children}
+          </animated.div>
+        )
+      }}
+    </Transition>
+  )
+}
+
 // export const wrapRootElement vs. exports.wrapRootElement...
 export const wrapRootElement = ({ element }) => {
   return (
     <ApolloProvider client={client}>
-      <Provider store={store}>{element}</Provider>
+      <Provider store={store}>
+        <RootTransitionWrapper>{element}</RootTransitionWrapper>
+      </Provider>
     </ApolloProvider>
   )
 }
+
+/**
+ * @TODO see if redux updates on pageElement mount/unmount is the way to go
+ */
+// const ReduxTransitionStateUpdater = props => {
+//   const dispatch = useDispatch()
+
+//   React.useEffect(() => {
+//     dispatch(setTransitionState(true))
+//     return () => dispatch(setTransitionState(false))
+//   })
+
+//   return props.children
+// }
+// export const wrapPageElement = ({ element, props }) => {
+//   return <ReduxTransitionStateUpdater>{element}</ReduxTransitionStateUpdater>
+// }
 
 /**
  * Fix Error:
  * `WebpackError: ReferenceError: IntersectionObserver is not defined`
  * https://github.com/gatsbyjs/gatsby/issues/10435#issuecomment-446627549
  */
-export const onClientEntry = async () => {
+export const onClientEntry = async (_, plugins) => {
   if (typeof IntersectionObserver === `undefined`) {
     await import(`intersection-observer`)
   }
@@ -51,7 +119,7 @@ export const onRouteUpdate = async (
     console.log("I think this page was already created:", err)
   }
 
-  client
+  await client
     .mutate({
       mutation: INCREMENT_VIEWS,
       variables: variables,
