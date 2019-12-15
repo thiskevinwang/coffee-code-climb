1c058b3a23aa3a240ca16169da530a8778cd44e0
Kevin Wang <kwangsan@gmail.com>
2019-12-15 09:20:14 -0500

    deprecate stuff in gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -14,53 +14,6 @@ import {
   TRACK_IP_VISITS,
 } from "./src/apollo"
 
-// const duration = 600
-// const defaultStyle = {
-//   transition: `opacity ${duration}ms ease-in-out`,
-//   opacity: 0,
-// }
-
-// const transitionStyles = {
-//   entering: { opacity: 1 },
-//   entered: { opacity: 1 },
-//   exiting: { opacity: 0 },
-//   exited: { opacity: 0 },
-// }
-
-// const RootTransitionWrapper = props => {
-//   const [inProp, setInProp] = React.useState(false)
-
-//   /**
-//    * @TODO Spring animate this
-//    */
-//   // const [{ opacity }, set] = useSpring(() => ({
-//   //   opacity: inProp ? 1 : 0,
-//   // }))
-
-//   React.useEffect(() => {
-//     setInProp(true)
-//     return () => {}
-//   }, [])
-
-//   return (
-//     <Transition timeout={duration} in={inProp} appear={true}>
-//       {state => {
-//         return (
-//           <animated.div
-//             // style={{ opacity }}
-//             style={{
-//               ...defaultStyle,
-//               ...transitionStyles[state],
-//             }}
-//           >
-//             {props.children}
-//           </animated.div>
-//         )
-//       }}
-//     </Transition>
-//   )
-// }
-
 /**
  * @TODO export this if context is needed elsewhere
  */
@@ -92,76 +45,70 @@ export const wrapRootElement = ({ element }) => {
 }
 
 /**
- * @TODO see if redux updates on pageElement mount/unmount is the way to go
- */
-// const ReduxTransitionStateUpdater = props => {
-//   const dispatch = useDispatch()
-
-//   React.useEffect(() => {
-//     dispatch(setTransitionState(true))
-//     return () => dispatch(setTransitionState(false))
-//   })
-
-//   return props.children
-// }
-// export const wrapPageElement = ({ element, props }) => {
-//   return <ReduxTransitionStateUpdater>{element}</ReduxTransitionStateUpdater>
-// }
-
-/**
  * Fix Error:
  * `WebpackError: ReferenceError: IntersectionObserver is not defined`
  * https://github.com/gatsbyjs/gatsby/issues/10435#issuecomment-446627549
  */
 export const onClientEntry = async (_, plugins) => {
-  try {
-    client.mutate({ mutation: TRACK_IP_VISITS })
-  } catch (error) {
-    console.error("TRACK_IP_VISITS", error)
-  }
+  /**
+   * @deprecated 12-15-2019
+   * - This code for an old apollo server -> DynamoDB, before I knew any DB logic.
+   * - Moving to a new apollo server -> RDS
+   */
+  // try {
+  //   client.mutate({ mutation: TRACK_IP_VISITS })
+  // } catch (error) {
+  //   console.error("TRACK_IP_VISITS", error)
+  // }
   if (typeof IntersectionObserver === `undefined`) {
     await import(`intersection-observer`)
   }
 }
 
 // fires when arriving and leaving...
-export const onRouteUpdate = async (
-  { location, prevLocation, ...rest },
-  plugins
-) => {
-  /**
-   * shared vars for client mutations
-   */
-  const variables = { id: 1, location: location.href }
 
-  try {
-    await client
-      .mutate({
-        mutation: CREATE_PAGE,
-        variables: variables,
-      })
-      .then(result => {})
-  } catch (err) {
-    console.error("I think this page was already created:", err)
-  }
+/**
+ * @deprecated 12-15-2019
+ * - This code for an old apollo server -> DynamoDB, before I knew any DB logic.
+ * - Moving to a new apollo server -> RDS
+ */
+// export const onRouteUpdate = async (
+//   { location, prevLocation, ...rest },
+//   plugins
+// ) => {
+//   /**
+//    * shared vars for client mutations
+//    */
+//   const variables = { id: 1, location: location.href }
 
-  try {
-    await client
-      .mutate({
-        mutation: INCREMENT_VIEWS,
-        variables: variables,
-      })
-      .then(result => {
-        console.log(
-          "INCREMENT_VIEWS",
-          location.href,
-          result.data.incrementViews.views
-        )
-      })
-  } catch (error) {
-    console.error("INCREMENT_VIEWS:", error)
-  }
-}
+//   try {
+//     await client
+//       .mutate({
+//         mutation: CREATE_PAGE,
+//         variables: variables,
+//       })
+//       .then(result => {})
+//   } catch (err) {
+//     console.error("I think this page was already created:", err)
+//   }
+
+//   try {
+//     await client
+//       .mutate({
+//         mutation: INCREMENT_VIEWS,
+//         variables: variables,
+//       })
+//       .then(result => {
+//         console.log(
+//           "INCREMENT_VIEWS",
+//           location.href,
+//           result.data.incrementViews.views
+//         )
+//       })
+//   } catch (error) {
+//     console.error("INCREMENT_VIEWS:", error)
+//   }
+// }
 
 /**
  * CONCURRENT MODE!!!
