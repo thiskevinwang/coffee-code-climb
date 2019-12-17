89f112dc7278a12d5c754f55c87b8c605076a01d
Kevin Wang <kwangsan@gmail.com>
2019-12-16 22:10:49 -0500

    swap out client for MY BETTER CLIENT

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,24 +1,54 @@
-import ApolloClient, { gql } from "apollo-boost"
 import { ApolloProvider } from "@apollo/react-hooks"
+import { HttpLink } from "apollo-link-http"
+import { WebSocketLink } from "apollo-link-ws"
+import { split } from "apollo-link"
+import { getMainDefinition } from "apollo-utilities"
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { gql } from "apollo-boost"
+import { SubscriptionClient } from "subscriptions-transport-ws"
+import * as ws from "ws"
+import { OperationDefinitionNode } from "graphql"
 import fetch from "isomorphic-fetch"
 
-/**
- * # client
- * Initialize the client
- */
-const client = new ApolloClient({
-  uri:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:4000/"
-      : "https://graphql-server-example.thekevinwang.now.sh/",
+const ENDPOINT = process.env.GATSBY_RDS_API_ENDPOINT
+const HTTPS_PROTOCOL = "https://"
+const WSS_PROTOCOL = "wss://"
+const QUERY_ENDPOINT = `${HTTPS_PROTOCOL}${ENDPOINT}`
+const SUBSCRIPTION_ENDPOINT = `${WSS_PROTOCOL}${ENDPOINT}`
+
+const subscriptionClient =
   /**
-   * Issue with `fetch` during gatsby's build process
-   * https://github.com/gatsbyjs/gatsby/issues/3650#issuecomment-410146046
+   * Passing `ws` as the 3rd constructor argument fixes this error when running `gatsby build`
+   * > WebpackError: Unable to find native implementation, or alternative implementation for WebSocket!
+   * @see https://github.com/apollographql/subscriptions-transport-ws/issues/191#issuecomment-311441663
    *
-   * > Whatever fetch apollo-boost uses does not work when Gatsby is compiling.
-   * > https://www.apollographql.com/docs/link/links/http/#fetch
+   * Error: ws does not work in the browser. Browser clients must use the native WebSocket object
+   * - so only add it when window is undefined, aka during build process / ssr
    */
-  fetch,
+  new SubscriptionClient(
+    SUBSCRIPTION_ENDPOINT,
+    { reconnect: true },
+    typeof window === "undefined" && ws
+  )
+
+const wsLink = new WebSocketLink(subscriptionClient)
+const httpLink = new HttpLink({ uri: QUERY_ENDPOINT })
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(
+      query
+    ) as OperationDefinitionNode
+    return kind === "OperationDefinition" && operation === "subscription"
+  },
+  wsLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
 })
 
 const GET_PAGE = gql`
