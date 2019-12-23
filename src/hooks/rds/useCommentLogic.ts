87db86244ef6e0f20ff1f65bf7dd42915a0e4e4e
Kevin Wang <kwangsan@gmail.com>
2019-12-22 20:36:52 -0500

    merge reactions into comments

--- src/hooks/rds/useCommentLogic.ts
+++ src/hooks/rds/useCommentLogic.ts
@@ -1,7 +1,9 @@
 import { useEffect, useReducer } from "react"
 import { useTransition, config } from "react-spring"
 import _ from "lodash"
-import { useFetchComments, Comment } from "hooks/rds/useFetchComments"
+
+import { useFetchComments } from "hooks/rds/useFetchComments"
+import { Reaction, Comment } from "entities"
 
 type Action =
   | {
@@ -12,6 +14,10 @@ type Action =
       type: "newComment"
       newComment: Comment
     }
+  | {
+      type: "newReaction"
+      newReaction: Reaction
+    }
 
 const sortByNewest = _.flow(_.partialRight(_.sortBy, "created"), _.reverse)
 
@@ -21,6 +27,53 @@ function commentsReducer(state: Comment[], action: Action): Comment[] {
       return sortByNewest(action.getAllComments)
     case "newComment":
       return sortByNewest([...state, action.newComment])
+    case "newReaction":
+      /**
+       * In the case of a newReaction subscription, we have to
+       * find the comment, in state, and create Left/Right slices
+       * of state,
+       * - [...left, comment, ...right]
+       *
+       * Take the comment's reactions, which is another array,
+       * and update that accordingly
+       *
+       * An incoming newReaction could match an existing reaction
+       * by id.
+       * - In that case, we update the `variant`, and return the array
+       *
+       * An incoming newReaction could also be brand new, and its
+       * id will not exist in the array.
+       * - In that case, we return a new array via `concat()`
+       *
+       */
+      const updatedCommentId = action.newReaction.comment.id
+      /* index of previous comment to replace with `newReactions` */
+      const i = _.findIndex(state, { id: updatedCommentId })
+      /* "left" and "right" of the previous comment array element */
+      const left = state.slice(0, i)
+      const right = state.slice(i + 1)
+      /* clone the comment whose reactions we want to update */
+      const comment = _.cloneDeep(state[i])
+
+      const match = _.findIndex(comment.reactions, {
+        id: action.newReaction.id,
+      })
+
+      let updatedReactions: Reaction[]
+      if (match < 0) {
+        updatedReactions = comment.reactions.concat(action.newReaction)
+      } else {
+        updatedReactions = _.map([...comment.reactions], (reaction, i) => {
+          if (reaction.id === action.newReaction.id) {
+            return action.newReaction
+          } else {
+            return reaction
+          }
+        })
+      }
+
+      const output = { ...comment, reactions: updatedReactions } as Comment
+      return sortByNewest([...left, output, ...right])
     default:
       return state
   }
@@ -35,8 +88,9 @@ function commentsReducer(state: Comment[], action: Action): Comment[] {
  */
 export function useCommentLogic({ isIntersecting } = { isIntersecting: true }) {
   const {
-    lazyQueryProps: [fetchComments, { called, loading, error, data }],
-    subscriptionProps,
+    getAllCommentsLazyQuery: [fetchComments, { called, loading, error, data }],
+    newCommentSubscription,
+    newReactionSubscription,
   } = useFetchComments()
   useEffect(() => {
     isIntersecting && fetchComments()
@@ -53,8 +107,8 @@ export function useCommentLogic({ isIntersecting } = { isIntersecting: true }) {
     return () => {}
   }, [loading, error, called])
   useEffect(() => {
-    if (!subscriptionProps.loading && !subscriptionProps.error) {
-      const newComment: Comment = subscriptionProps.data.newComment
+    if (!newCommentSubscription.loading && !newCommentSubscription.error) {
+      const newComment: Comment = newCommentSubscription.data.newComment
       if (newComment) {
         dispatch({
           type: "newComment",
@@ -63,7 +117,23 @@ export function useCommentLogic({ isIntersecting } = { isIntersecting: true }) {
       }
     }
     return () => {}
-  }, [subscriptionProps.loading, subscriptionProps.data?.newComment])
+  }, [newCommentSubscription.loading, newCommentSubscription.data?.newComment])
+
+  useEffect(() => {
+    if (!newReactionSubscription.loading && !newReactionSubscription.error) {
+      const newReaction = newReactionSubscription.data.newReaction
+      if (newReaction) {
+        dispatch({
+          type: "newReaction",
+          newReaction: newReaction,
+        })
+      }
+    }
+    return () => {}
+  }, [
+    newReactionSubscription.loading,
+    newReactionSubscription.data?.newReaction,
+  ])
 
   const transition = useTransition(
     comments,
