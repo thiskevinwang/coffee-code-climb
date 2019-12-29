e8282c0698aeed3a6e2df40c5f6038d81b0192b3
Kevin Wang <kwangsan@gmail.com>
2019-12-29 00:08:43 -0500

    clean up `useLazyPolling`

--- src/hooks/apollo/useLazyPolling.ts
+++ src/hooks/apollo/useLazyPolling.ts
@@ -1,10 +1,38 @@
 import { useEffect } from "react"
+import ms from "ms"
 
 interface IPollingOptions {
+  /**
+   * This function sets up an interval in ms and fetches
+   * the query each time the specified interval passes.
+   *
+   * @note This is in the `QueryResult<TData, TVariables>` object
+   */
   startPolling(pollInterval: number): void
+
+  /**
+   * This function stops the query from polling.
+   */
   stopPolling(): void
-  interval: number
+
+  /**
+   * A boolean indicating if the query function has been called,
+   * used by `useLazyQuery` (not set for `useQuery` / `Query`).
+   */
   called: boolean
+
+  /**
+   * Specifies the interval in ms at which you want your
+   * component to poll for data.If not specified, the query
+   * will fire every **1 second**.
+   */
+  interval?: number
+
+  /**
+   * The amount of ms after which polling will stop. If not
+   * specified, polling will stop after **5 minutes**.
+   */
+  stopAfter?: number
 }
 /**
  * This hook is used to handle polling on window focus/blur,
@@ -18,26 +46,19 @@ interface IPollingOptions {
 export function useLazyPolling({
   startPolling,
   stopPolling,
-  interval,
   called,
+  interval,
+  stopAfter,
 }: IPollingOptions) {
   useEffect(() => {
-    // const handleStartPolling = () => {
-    //   startPolling?.(interval)
-    // }
-    // const handleStopPolling = () => {
-    //   stopPolling?.()
-    // }
-
     if (called) {
-      startPolling?.(interval)
-      // window.addEventListener("blur", handleStopPolling)
-      // window.addEventListener("focus", handleStartPolling)
+      startPolling?.(interval ?? 1000)
+      setTimeout(() => {
+        stopPolling?.()
+      }, stopAfter ?? ms("5m"))
     }
     return () => {
       stopPolling?.()
-      // window.removeEventListener("blur", handleStopPolling)
-      // window.removeEventListener("focus", handleStartPolling)
     }
   }, [startPolling, stopPolling, interval, called])
 }
