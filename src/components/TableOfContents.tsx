e8333f30b8b83355b4d6573792c412429a718877
Kevin Wang <kwangsan@gmail.com>
2020-02-03 00:17:06 -0500

    slight refator

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -51,46 +51,47 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ title, __html }) => {
 
       set({
         top: scrollTop + 50,
-        // onFrame: props => {
-        //   window.scroll(0, scrollTop)
-        // },
       })
     },
     { domTarget: typeof window !== "undefined" && window }
   )
   useEffect(bindScrollGesture, [bindScrollGesture])
 
-  // console.time()
   const ast: [ASTNode] = useCallback(parse(__html), [])
 
-  const helper = useCallback(
-    (nodes: (ASTNode | LeafNode)[], depth = 0): React.ReactNode => {
+  /**
+   * # generateTree
+   * Recursive helper function, used to traverse the Abstract Syntax Tree returned by
+   * { parse } from "html-parse-stringify", and then generate
+   * a React Tree.
+   *
+   * @param nodes the ast returned by `html-parse-stringify`
+   * @param depth just for fun
+   */
+  const generateTree = useCallback(
+    (nodes: (ASTNode | LeafNode)[], depth: number = 0): React.ReactNode => {
       return nodes.map(node => {
         if (!node.children) return node.content
-        // console.log(node)s
 
         // intercept "<a>" nodes, and create a gatsby Link instead
         if (node.name === "a") {
           const hash = node.attrs.href
             .replace(window.location.pathname, "")
             .replace("#", "")
+
           // this needs run after useEffect, aka after the browser
           // gets a chance to paint
-          const anchor = document.getElementById(hash)
+          const anchorEl = document.getElementById(hash)
+          const offset = anchorEl?.offsetTop
+
           return React.createElement(
             /** React.createElement(TYPE, _, _) */
-            Link,
+            animated.a,
             /** React.createElement(_, PROPS, _) */
             {
+              ...node.attrs,
               /**
-               * node.attrs.href: "/feb-2-2020/#reminders-to-myself"
-               * window.location.pathname: "/feb-2-2020/"
-               * window.location.hash: "#reminders-to-myself"
-               */
-              to: node.attrs.href,
-              activeStyle: { color: "red" },
-              /**
-               * style will only be instantiated, and not dynamically updated
+               * @TODO style will only apply initially, and will not be dynamically updated
                */
               // style: {
               //   fontSize:
@@ -100,29 +101,14 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ title, __html }) => {
               //       : "",
               // },
               onClick: e => {
-                // prevent jumping
-                // but also prevents updating window hash
+                // prevent jumping - but also prevents updating window hash
                 e.preventDefault()
-
-                // update the hash...
-                const hash = e.target.href
-                  ?.replace(window.location.origin, "")
-                  .replace(window.location.pathname, "")
-                  .replace("#", "")
                 // https://stackoverflow.com/questions/3870057/how-can-i-update-window-location-hash-without-jumping-the-document
-                if (history.pushState) {
-                  // IE10, Firefox, Chrome, etc.
-                  window.history.pushState(null, null, "#" + hash)
-                } else {
-                  // IE9, IE8, etc
-                  window.location.hash = hash
-                }
-
-                // find the anchor element that corresponds to the hash
-                const anchor = document.getElementById(hash)
-                const offset = anchor?.offsetTop
-
-                // scroll to that element
+                history.replaceState
+                  ? // IE10, Firefox, Chrome, etc
+                    window.history.replaceState(null, null, "#" + hash)
+                  : // IE9, IE8, etc
+                    (window.location.hash = hash)
                 window.scrollTo({
                   top: offset,
                   behavior: "smooth",
@@ -130,14 +116,14 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ title, __html }) => {
               },
             },
             /** React.createElement(_, _, CHILDREN) */
-            helper(node.children, depth + 1)
+            generateTree(node.children, depth + 1)
           )
         }
 
         return React.createElement(
           node.name,
           node.attrs,
-          helper(node.children, depth + 1)
+          generateTree(node.children, depth + 1)
         )
       })
     },
@@ -151,9 +137,8 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ title, __html }) => {
      * chance to paint. This way we can do trickery like 'window.getDocumentById(hash)'
      * from within the `helper` function.
      */
-    setGenerated(helper(ast))
+    setGenerated(generateTree(ast))
   }, [])
-  // const generated = helper(ast)
 
   return (
     <Container style={{ top, zIndex: 10 }}>
