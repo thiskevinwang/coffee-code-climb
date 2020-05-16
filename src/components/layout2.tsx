00a1075ef05b53a721d8070e9a98d2e0b5b9ef32
Kevin Wang <kwangsan@gmail.com>
2020-05-16 12:29:04 -0400

    memo() layout2

--- src/components/layout2.tsx
+++ src/components/layout2.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { memo } from "react"
 import { useSelector } from "react-redux"
 import { useSpring, animated } from "react-spring"
 import { compose } from "redux"
@@ -105,4 +105,4 @@ function Layout({ location, title, children }) {
   )
 }
 
-export default compose(withSVGTrail)(Layout)
+export default compose(withSVGTrail, memo)(Layout)
