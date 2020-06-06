298d59cc6c960b736a97bf50e703a0f53750901b
Kevin Wang <kwangsan@gmail.com>
2020-06-05 22:53:01 -0400

    V1 of thumbup feature - only GET - no POST yet

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -4,6 +4,7 @@ import _ from "lodash"
 import styled from "styled-components"
 import theme from "styled-theming"
 import { animated } from "react-spring"
+import { Skeleton } from "@material-ui/lab"
 
 // Components
 import Bio from "components/bio"
@@ -13,10 +14,12 @@ import { LayoutManager } from "components/layoutManager"
 import { PrevNextNavigation } from "components/TemplateComponents"
 import { CreateComment } from "components/Comments/Create"
 import { CommentsByUrl } from "components/Comments/Display/ByUrl"
+import { Tag } from "components/Posts/V1"
 
 // Other
 import { rhythm, scale } from "utils/typography"
 import * as Colors from "consts/Colors"
+import { useFetch } from "hooks/useFetch"
 
 export const Hr = styled(animated.div)`
   min-height: 1px;
@@ -27,10 +30,93 @@ export const Hr = styled(animated.div)`
   })};
 `
 
+const ClapsFixedContainer = styled(animated.div)`
+  --blog-width: 42rem;
+  --blog-padding-x: 1.3125rem;
+  --max-width: 1192px;
+
+  /* allow click through */
+  pointer-events: none;
+
+  margin-left: calc(
+    (min(100vw, var(--max-width)) - var(--blog-width) + var(--blog-padding-x)) /
+      -2
+  );
+
+  position: fixed;
+  top: 30%;
+  height: 50px;
+  width: 100vw;
+  max-width: var(--max-width);
+
+  > div {
+    /* capture clicks */
+    pointer-events: all;
+    span {
+      user-select: none;
+      margin-right: 1rem;
+    }
+    > p {
+      display: flex;
+      justify-content: center;
+      margin: 0;
+    }
+    width: 145px;
+    padding: 1rem;
+
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    border-color: ${theme("mode", {
+      light: Colors.greyLighter,
+      dark: Colors.greyDarker,
+    })};
+  }
+
+  color: ${theme("mode", {
+    light: Colors.black,
+    dark: Colors.silver,
+  })};
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
+`
+
+const ClapsLayoutContainer = styled(animated.div)`
+  > div {
+    span {
+      user-select: none;
+      margin-right: 1rem;
+    }
+    > p {
+      display: flex;
+      margin: 0;
+    }
+    padding: 1rem;
+
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    border-color: ${theme("mode", {
+      light: Colors.greyLighter,
+      dark: Colors.greyDarker,
+    })};
+
+    margin-bottom: 1rem;
+  }
+`
+
 export default function BlogPostTemplate({ data, pageContext, location }) {
   const post = data.markdownRemark
   const { title: siteTitle } = data.site.siteMetadata
   const { previous, next, postTitle, tableOfContents } = pageContext
+
+  const URI = `${process.env.GATSBY_LAMBDA_ENDPOINT}/sandbox/claps?slug=${location.pathname}`
+  const { data: res, error } = useFetch<GetClapsResponse, any>(URI)
+  const isLoading = !res || !error
+
+  const handleClick = () => {}
 
   return (
     <LayoutManager location={location} title={siteTitle}>
@@ -59,19 +145,44 @@ export default function BlogPostTemplate({ data, pageContext, location }) {
 
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
 
-      <small>
-        Tags:{" "}
+      <div style={{ marginBottom: `2rem` }}>
         {post.frontmatter.tags.map((tag, index) => (
-          <Link
-            to={`/tags/${_.kebabCase(tag)}/`}
-            style={{ color: "#A6B1BB", margin: 3 }}
-            key={index}
-          >
-            {tag}
-          </Link>
+          <Tag>
+            <Link to={`/tags/${_.kebabCase(tag)}/`} key={index}>
+              {tag}
+            </Link>
+          </Tag>
         ))}
-      </small>
-      <hr
+      </div>
+      <ClapsLayoutContainer>
+        <div>
+          {!isLoading ? (
+            <Skeleton animation="wave"></Skeleton>
+          ) : (
+            <>
+              <p>
+                <span>{res?.Item?.claps.N ?? 0}</span>
+                <ThumsUp onClick={handleClick} />
+              </p>
+            </>
+          )}
+        </div>
+      </ClapsLayoutContainer>
+      <ClapsFixedContainer>
+        <div>
+          {!isLoading ? (
+            <Skeleton animation="wave"></Skeleton>
+          ) : (
+            <>
+              <p>
+                <span>{res?.Item?.claps.N ?? 0}</span>
+                <ThumsUp onClick={handleClick} />
+              </p>
+            </>
+          )}
+        </div>
+      </ClapsFixedContainer>
+      <Hr
         style={{
           marginBottom: rhythm(1),
         }}
@@ -108,3 +219,59 @@ export const pageQuery = graphql`
     }
   }
 `
+
+interface GetClapsResponse {
+  Item: Item
+}
+
+interface Item {
+  PK: Key
+  SK: Key
+  claps: Claps
+}
+
+interface Claps {
+  N: string
+}
+
+interface Key {
+  S: string
+}
+
+const Svg = styled.svg`
+  cursor: pointer;
+  transition: color 50ms ease-in-out, transform 100ms ease-in-out;
+  will-change: color;
+  :hover&:not(:active) {
+    color: ${theme("mode", {
+      light: Colors.greyLighter,
+      dark: Colors.greyDarker,
+    })};
+  }
+  :active {
+    color: ${theme("mode", {
+      light: Colors.greyDarker,
+      dark: Colors.greyLighter,
+    })};
+    transform: scale(1.2);
+  }
+`
+
+const ThumsUp = () => {
+  return (
+    <Svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+      shapeRendering="geometricPrecision"
+      // style="color:var(--geist-foreground)"
+    >
+      <path d="M14 9V5a3 3 0 00-3-3l-4 9v11h11.28a2 2 0 002-1.7l1.38-9a2 2 0 00-2-2.3zM7 22H4a2 2 0 01-2-2v-7a2 2 0 012-2h3" />
+    </Svg>
+  )
+}
