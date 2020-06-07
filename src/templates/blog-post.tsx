456b49a05f264088ab986aeffc706041ae3d23c6
Kevin Wang <kwangsan@gmail.com>
2020-06-07 14:48:59 -0400

    animated layout claps

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,13 +1,11 @@
-
-import React, { useState, memo, useEffect, useLayoutEffect } from "react"
+import React, { useState, memo } from "react"
 import { Link, graphql } from "gatsby"
 import _ from "lodash"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import theme from "styled-theming"
 import { animated, useTransition } from "react-spring"
 import { Skeleton } from "@material-ui/lab"
 import uuid from "uuid"
-
 
 // Components
 import Bio from "components/bio"
@@ -24,99 +22,6 @@ import { rhythm, scale } from "utils/typography"
 import * as Colors from "consts/Colors"
 import { useFetch } from "hooks/useFetch"
 import { useOptimisticClaps } from "hooks/useOptimisticClaps"
-
-export const Hr = styled(animated.div)`
-  min-height: 1px;
-  margin-bottom: 2px;
-  background: ${theme("mode", {
-    light: Colors.greyLighter,
-    dark: Colors.greyDarker,
-  })};
-`
-
-const Claps = styled(animated.div)`
-  position: relative;
-`
-const ClapsFixedContainer = styled(animated.div)`
-  --blog-width: 42rem;
-  --blog-padding-x: 1.3125rem;
-  --max-width: 1192px;
-
-  /* allow click through */
-  pointer-events: none;
-
-  margin-left: calc(
-    (min(100vw, var(--max-width)) - var(--blog-width) + var(--blog-padding-x)) /
-      -2
-  );
-
-  position: fixed;
-  top: 30%;
-  height: 50px;
-  width: 100vw;
-  max-width: var(--max-width);
-
-  ${Claps} {
-    /* capture clicks */
-    pointer-events: all;
-    span {
-      user-select: none;
-      margin-right: 1rem;
-    }
-    > p,
-    > small {
-      user-select: none;
-      display: flex;
-      justify-content: center;
-      margin: 0;
-    }
-    width: 145px;
-    padding: 1rem;
-
-    border-style: solid;
-    border-width: 1px;
-    border-radius: 5px;
-    border-color: ${theme("mode", {
-      light: Colors.greyLighter,
-      dark: Colors.greyDarker,
-    })};
-  }
-
-  color: ${theme("mode", {
-    light: Colors.black,
-    dark: Colors.silver,
-  })};
-
-  @media (max-width: 1200px) {
-    display: none;
-  }
-`
-
-const ClapsLayoutContainer = styled(animated.div)`
-  ${Claps} {
-    span {
-      user-select: none;
-      margin-right: 1rem;
-    }
-    > p,
-    > small {
-      user-select: none;
-      display: flex;
-      margin: 0;
-    }
-    padding: 1rem;
-
-    border-style: solid;
-    border-width: 1px;
-    border-radius: 5px;
-    border-color: ${theme("mode", {
-      light: Colors.greyLighter,
-      dark: Colors.greyDarker,
-    })};
-
-    margin-bottom: 1rem;
-  }
-`
 
 const URI = `${process.env.GATSBY_LAMBDA_ENDPOINT}/sandbox/claps`
 
@@ -192,6 +97,13 @@ export default function BlogPostTemplate({ data, pageContext, location }) {
       </div>
       <ClapsLayoutContainer>
         <Claps>
+          {transitions.map(({ item, props, key }) => (
+            <Remover>
+              <PlusCounter key={key} style={props} widthPx={100}>
+                +1
+              </PlusCounter>
+            </Remover>
+          ))}
           {isLoading ? (
             <Skeleton animation="wave"></Skeleton>
           ) : (
@@ -211,7 +123,7 @@ export default function BlogPostTemplate({ data, pageContext, location }) {
       <ClapsFixedContainer>
         <Claps>
           {transitions.map(({ item, props, key }) => (
-            <Remover item={item} setItems={setItems}>
+            <Remover>
               <PlusCounter key={key} style={props}>
                 +1
               </PlusCounter>
@@ -323,7 +235,6 @@ const ThumsUp = memo(() => {
       strokeLinejoin="round"
       fill="none"
       shapeRendering="geometricPrecision"
-      // style={{ transform: radians.interpolate(interp(0)) }}
       // style="color:var(--geist-foreground)"
     >
       <path d="M14 9V5a3 3 0 00-3-3l-4 9v11h11.28a2 2 0 002-1.7l1.38-9a2 2 0 00-2-2.3zM7 22H4a2 2 0 01-2-2v-7a2 2 0 012-2h3" />
@@ -341,12 +252,12 @@ const PlusCounter = styled(animated.div)`
     light: "var(--geist-cyan)",
     dark: "var(--geist-purple)",
   })};
-  width: 100%;
+  width: ${(p) => (p.widthPx ? `${p.widthPx}px` : `100%`)};
   text-align: center;
   position: absolute;
 `
 
-const Remover = memo(({ children, setItems, item }) => {
+const Remover = memo(({ children }) => {
   // useLayoutEffect(() => {
   //   const timeout = setTimeout(() => {
   //     setItems((items) => items.filter((e) => e.id !== item.id))
@@ -357,3 +268,96 @@ const Remover = memo(({ children, setItems, item }) => {
   // }, [item])
   return <>{children}</>
 })
+
+export const Hr = styled(animated.div)`
+  min-height: 1px;
+  margin-bottom: 2px;
+  background: ${theme("mode", {
+    light: Colors.greyLighter,
+    dark: Colors.greyDarker,
+  })};
+`
+
+const Claps = styled(animated.div)`
+  position: relative;
+`
+const ClapsFixedContainer = styled(animated.div)`
+  --blog-width: 42rem;
+  --blog-padding-x: 1.3125rem;
+  --max-width: 1192px;
+
+  /* allow click through */
+  pointer-events: none;
+
+  margin-left: calc(
+    (min(100vw, var(--max-width)) - var(--blog-width) + var(--blog-padding-x)) /
+      -2
+  );
+
+  position: fixed;
+  top: 30%;
+  height: 50px;
+  width: 100vw;
+  max-width: var(--max-width);
+
+  ${Claps} {
+    /* capture clicks */
+    pointer-events: all;
+    span {
+      user-select: none;
+      margin-right: 1rem;
+    }
+    > p,
+    > small {
+      user-select: none;
+      display: flex;
+      justify-content: center;
+      margin: 0;
+    }
+    width: 145px;
+    padding: 1rem;
+
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    border-color: ${theme("mode", {
+      light: Colors.greyLighter,
+      dark: Colors.greyDarker,
+    })};
+  }
+
+  color: ${theme("mode", {
+    light: Colors.black,
+    dark: Colors.silver,
+  })};
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
+`
+
+const ClapsLayoutContainer = styled(animated.div)`
+  ${Claps} {
+    span {
+      user-select: none;
+      margin-right: 1rem;
+    }
+    > p,
+    > small {
+      user-select: none;
+      display: flex;
+      margin: 0;
+    }
+    padding: 1rem;
+
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    border-color: ${theme("mode", {
+      light: Colors.greyLighter,
+      dark: Colors.greyDarker,
+    })};
+
+    margin-bottom: 1rem;
+  }
+`
