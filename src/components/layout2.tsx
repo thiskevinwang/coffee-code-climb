3cc5a531d3bdbad71b3dd435d35edddaa5e45223
Kevin Wang <kwangsan@gmail.com>
2020-06-17 23:21:26 -0400

    Blobs + Create global styles directory (#148)
    * gatsby-plugin-alias-imports:
      - add `globalStyles` directory
    * extract browser/ssr globalStyles
    * replace dots with blob bg
    
    ![](https://user-images.githubusercontent.com/26389321/84964253-a3293500-b0d9-11ea-8944-e03d066aa264.png)

--- src/components/layout2.tsx
+++ src/components/layout2.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react"
 import { useSelector } from "react-redux"
-import { useSpring, animated } from "react-spring"
+import { useSpring, animated, OpaqueInterpolation } from "react-spring"
 import { compose } from "redux"
 import { Link } from "gatsby"
 import styled from "styled-components"
@@ -10,7 +10,7 @@ import { NavBar2 } from "components/Layout2Components/NavBar2"
 import { useWindowScrollPercent } from "hooks/useWindowScrollPercent"
 
 import { rhythm } from "utils/typography"
-import * as Colors from "consts/Colors"
+import { Colors } from "consts/Colors"
 import Footer from "./Footer"
 
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
@@ -24,31 +24,6 @@ const ThemedBackground = styled(animated.div)`
   z-index: -9000;
 `
 
-const Dots = styled(animated.div)`
-  position: fixed;
-  top: 0;
-  right: 0;
-  height: 100%;
-  width: 30%;
-  transform: skewX(6deg);
-  transform-origin: top right;
-  z-index: -8998;
-
-  background-image: -webkit-repeating-radial-gradient(
-    center center,
-    ${(props) =>
-      !props.isDarkMode ? `rgba(255, 255, 255, 0.8)` : `rgba(0, 0, 0, 0.5)`},
-    ${(props) =>
-        !props.isDarkMode ? `rgba(255, 255, 255, 0.8)` : `rgba(0, 0, 0, 0.5)`}
-      1px,
-    transparent 1px,
-    transparent 100%
-  );
-  -webkit-background-size: 15px 15px;
-  -moz-background-size: 15px 15px;
-  background-size: 15px 15px;
-`
-
 function Layout({ location, title, children }) {
   const rootPath: string = `${__PATH_PREFIX__}/`
   const topLink =
@@ -57,25 +32,17 @@ function Layout({ location, title, children }) {
   const isDarkMode = useSelector((state: any) => state.isDarkMode)
 
   const { background } = useSpring({
-    background: isDarkMode ? Colors.blackDarker : Colors.silverLighter,
+    background: isDarkMode ? Colors.BLACK_DARKER : Colors.SILVER_LIGHTER,
   })
 
   const [scrollYPercent] = useWindowScrollPercent()
 
   return (
     <>
-      <Dots />
-      <ThemedBackground
-        style={{
-          background,
-          // background: scrollYPercent.interpolate({
-          //   range: [0, 1],
-          //   output: isDarkMode
-          //     ? [Colors.blackLighter, Colors.blackDarker]
-          //     : [Colors.silverLighter, Colors.silverDarker],
-          // }),
-        }}
-      />
+      <BlobHolder>
+        <Blob y={scrollYPercent} />
+      </BlobHolder>
+      <ThemedBackground style={{ background }} />
       <NavBar2 />
 
       <animated.div
@@ -106,3 +73,44 @@ function Layout({ location, title, children }) {
 }
 
 export default compose(memo)(Layout)
+
+const BlobHolder = styled.div`
+  display: flex;
+  align-items: flex-end;
+  position: fixed;
+  top: 10%;
+  left: -50%;
+  height: 100vh;
+  width: 150vw;
+  min-width: 800px;
+  z-index: -8000;
+  opacity: 0.25;
+  pointer-events: none;
+`
+
+interface BlobProps {
+  y: OpaqueInterpolation<number>
+}
+
+// complexity 12
+// https://blobs.app/
+const D1 =
+  "M438,300.5Q424,351,376.5,369.5Q329,388,289.5,427.5Q250,467,194.5,455Q139,443,105.5,398Q72,353,83.5,301.5Q95,250,81,197Q67,144,125.5,140Q184,136,217,85Q250,34,310.5,37Q371,40,402,92Q433,144,442.5,197Q452,250,438,300.5Z"
+const D2 =
+  "M451,308Q450,366,380,359.5Q310,353,280,365Q250,377,203,394.5Q156,412,134,371Q112,330,122.5,290Q133,250,112,204Q91,158,119.5,116Q148,74,199,103.5Q250,133,307,93Q364,53,361.5,120Q359,187,405.5,218.5Q452,250,451,308Z"
+const D3 =
+  "M385,276.5Q341,303,348,367.5Q355,432,302.5,391Q250,350,205.5,377Q161,404,123,374.5Q85,345,102.5,297.5Q120,250,135,221Q150,192,138,114Q126,36,188,38.5Q250,41,308.5,44Q367,47,373.5,111Q380,175,404.5,212.5Q429,250,385,276.5Z"
+
+const Blob = ({ y }: BlobProps) => {
+  return (
+    <animated.svg viewBox="0 0 500 500" width="100%">
+      <animated.path
+        d={y.interpolate({ range: [0, 0.5, 1], output: [D1, D2, D3] })}
+        fill={y.interpolate({
+          range: [0, 0.33, 0.66, 1],
+          output: [Colors.VIOLET, Colors.ALERT, Colors.PURPLE, Colors.CYAN],
+        })}
+      ></animated.path>
+    </animated.svg>
+  )
+}
