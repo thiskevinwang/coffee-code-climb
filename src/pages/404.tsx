e3ae41be859479a59c1f3a3ea6421169f3a7441f
Kevin Wang <kwangsan@gmail.com>
2020-06-24 09:28:05 -0400

    fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
+import { LayoutManager } from "components/layoutManager"
 import SEO from "../components/seo"
 
 class NotFoundPage extends React.Component {
@@ -10,11 +10,11 @@ class NotFoundPage extends React.Component {
     const siteTitle = data.site.siteMetadata.title
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <LayoutManager location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
+      </LayoutManager>
     )
   }
 }
