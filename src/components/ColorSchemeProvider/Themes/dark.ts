fa83c4823c6374d84c8716a8b0580f432bf77eeb
Kevin Wang <kwangsan@gmail.com>
2020-10-24 13:29:36 -0400

    refacor Colors usage in Themes

--- src/components/ColorSchemeProvider/Themes/dark.ts
+++ src/components/ColorSchemeProvider/Themes/dark.ts
@@ -1,25 +1,25 @@
-import * as Colors from "consts/Colors"
+import { Colors } from "consts/Colors"
 import type { ThemeType } from "./type"
 
 const formInput = {
-  background: Colors.blackDarker,
-  borderColorBase: Colors.greyDarker,
-  borderColorFocus: Colors.silverLighter,
+  background: Colors.BLACK_DARKER,
+  borderColorBase: Colors.GREY_DARKER,
+  borderColorFocus: Colors.SILVER_LIGHTER,
   borderColorError: "#a00",
-  color: Colors.silverLighter,
+  color: Colors.SILVER_LIGHTER,
 }
 
 const formButton = {
-  background: Colors.silverLighter,
-  backgroundDisabled: Colors.blackDarker,
-  backgroundHover: Colors.blackDarker,
-  borderColorDisabled: Colors.greyDarker,
-  color: Colors.blackDarker,
-  colorHover: Colors.silverLighter,
+  background: Colors.SILVER_LIGHTER,
+  backgroundDisabled: Colors.BLACK_DARKER,
+  backgroundHover: Colors.BLACK_DARKER,
+  borderColorDisabled: Colors.GREY_DARKER,
+  color: Colors.BLACK_DARKER,
+  colorHover: Colors.SILVER_LIGHTER,
 }
 
 const commentRenderer = {
-  borderColor: Colors.greyDarker,
+  borderColor: Colors.GREY_DARKER,
 }
 
 export const darkTheme: ThemeType = {
--- src/components/ColorSchemeProvider/Themes/light.ts
+++ src/components/ColorSchemeProvider/Themes/light.ts
@@ -1,25 +1,25 @@
-import * as Colors from "consts/Colors"
+import { Colors } from "consts/Colors"
 import type { ThemeType } from "./type"
 
 const formInput = {
-  background: Colors.silverLighter,
-  borderColorBase: Colors.greyLighter,
-  borderColorFocus: Colors.blackDarker,
+  background: Colors.SILVER_LIGHTER,
+  borderColorBase: Colors.GREY_LIGHTER,
+  borderColorFocus: Colors.BLACK_DARKER,
   borderColorError: "#e00",
-  color: Colors.blackDarker,
+  color: Colors.BLACK_DARKER,
 }
 
 const formButton = {
-  background: Colors.blackDarker,
-  backgroundDisabled: Colors.silverLighter,
-  backgroundHover: Colors.silverLighter,
-  borderColorDisabled: Colors.greyLighter,
-  color: Colors.silverLighter,
-  colorHover: Colors.blackDarker,
+  background: Colors.BLACK_DARKER,
+  backgroundDisabled: Colors.SILVER_LIGHTER,
+  backgroundHover: Colors.SILVER_LIGHTER,
+  borderColorDisabled: Colors.GREY_LIGHTER,
+  color: Colors.SILVER_LIGHTER,
+  colorHover: Colors.BLACK_DARKER,
 }
 
 const commentRenderer = {
-  borderColor: Colors.greyLighter,
+  borderColor: Colors.GREY_LIGHTER,
 }
 
 export const lightTheme: ThemeType = {
