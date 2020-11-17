f8ff20ec1d65e593b21c1609230e6b652143ec30
Kevin Wang <kwangsan@gmail.com>
2020-11-17 05:10:11 -0500

    Pimp out LoadingIndicator

--- src/components/LoadingIndicator/index.tsx
+++ src/components/LoadingIndicator/index.tsx
@@ -1,46 +1,53 @@
-import React, { useReducer, useEffect } from "react"
+import React, { useReducer, useEffect, FC, CSSProperties } from "react"
+import styled, { BaseProps } from "styled-components"
+import theme from "styled-theming"
+
 // https://en.wiktionary.org/wiki/%E2%A0%BC
 const ICONS = ["⠇", "⠋", "⠙", "⠸", "⠴", "⠦"]
-const COLORS = ["red", "orange", "goldenyellow", "green", "blue", "purple"]
+const COLORS_LIGHT = [
+  "#ee0000",
+  "#f5a623",
+  "#50e3c2",
+  "#0070f3",
+  "#7928ca",
+  "#f81ce5",
+]
+const COLORS_DARK = [
+  "#ff1a1a",
+  "#f7b955",
+  "#79ffe1",
+  "#3291ff",
+  "#8a63d2",
+  "#ff0080",
+]
+
+interface Props {
+  style?: CSSProperties
+}
+
+const Spinner = styled.div<Props & { interval: number }>`
+  color: ${(p) =>
+    theme("mode", {
+      light: COLORS_LIGHT[p.interval],
+      dark: COLORS_DARK[p.interval],
+    })};
+  text-shadow: var(--shadow);
+`
 
 /**
  * # LoadingIndicator
  */
-export const LoadingIndicator = ({ style = {} }) => {
-  const [i, spin] = useReducer(s => (s + 1) % 6, 0)
+export const LoadingIndicator: FC<Props> = ({ style = {} }) => {
+  const [i, spin] = useReducer((s) => (s + 1) % 6, 0)
   useEffect(() => {
     const interval = setInterval(spin, 50)
     return () => {
       clearInterval(interval)
     }
   }, [])
-
-  const [showMessage, setShowMessageTrue] = useReducer(s => true, false)
-  useEffect(() => {
-    const timeout = setTimeout(setShowMessageTrue, 2000)
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [])
-
-  const reloadPage = e => {
-    window?.location.reload(true)
-  }
-
-  const linkProps = {
-    href: "#",
-    onClick: reloadPage,
-  }
-
   return (
-    <>
-      <div style={{ ...style, color: COLORS[i] }}>{ICONS[i]}</div>
-      {showMessage && (
-        <p>
-          Sorry, this is taking a while to load...{" "}
-          <a {...linkProps}>Reload the page</a>
-        </p>
-      )}
-    </>
+    <Spinner style={{ ...style }} interval={i}>
+      {ICONS[i]}
+    </Spinner>
   )
 }
