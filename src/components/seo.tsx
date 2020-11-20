7b998a3de168415b8756006ea33c78892522b3b0
Kevin Wang <kwangsan@gmail.com>
2020-11-20 01:27:56 -0500

    Improve SEO types

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,11 +6,23 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, keywords, title }) {
+interface Props {
+  description?: string
+  lang?: string
+  meta?: any[]
+  keywords?: string[]
+  title: string
+}
+function SEO({
+  description,
+  lang = `en`,
+  meta = [],
+  keywords = [],
+  title,
+}: Props) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -87,20 +99,6 @@ function SEO({ description, lang, meta, keywords, title }) {
         .concat(meta)}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  keywords: [],
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.array,
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
