e7098383f9a4f5def5d716390ee3f39b574a291e
Kevin Wang <kwangsan@gmail.com>
2020-11-22 20:35:54 -0500

    add Moon & Sun geist icons

---
+++ src/icons/moon.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export const Moon = () => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    fill="none"
+    shapeRendering="geometricPrecision"
+    style={{ color: "var(--geist-foreground)" }}
+  >
+    <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+  </svg>
+)
