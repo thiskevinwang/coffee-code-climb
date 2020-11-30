ff51c5f88d9fe9717e3b9dd687887f29e305bb3e
Kevin Wang <kwangsan@gmail.com>
2020-11-29 20:47:37 -0500

    update SubmitButton style

--- src/components/Form/SubmitButton.tsx
+++ src/components/Form/SubmitButton.tsx
@@ -1,19 +1,21 @@
-import styled, { BaseProps } from "styled-components"
+import styled from "styled-components"
 import { animated } from "react-spring"
 
 export const SubmitButton = styled(animated.button)`
   background: var(--accents-8);
   border-color: var(--accents-8);
   border-width: 1px;
-  border-radius: 0.25rem;
+  border-radius: var(--geist-radius);
   border-style: solid;
   color: var(--accents-1);
-  width: 7rem;
+  cursor: pointer;
+  font-size: 14px;
   height: var(--geist-form-small-height);
   margin-bottom: 2rem;
+  min-width: 80px;
   /* outline: none; */
-  transition: background 150ms ease-in-out, border-color 150ms ease-in-out,
-    color 150ms ease-in-out;
+  transition: all 200ms ease-in-out;
+  user-select: none;
   will-change: background, border-color, color;
 
   :hover,
@@ -23,8 +25,9 @@ export const SubmitButton = styled(animated.button)`
   }
 
   :disabled {
-    background: var(--accents-3);
-    border-color: var(--accents-6);
-    color: var(--accents-6);
+    background: var(--accents-1);
+    border-color: var(--accents-2);
+    color: var(--accents-4);
+    cursor: not-allowed;
   }
 `
