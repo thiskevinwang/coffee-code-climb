7e45cfab89b231de7c8c409c2cc4400558bac931
Kevin Wang <kwangsan@gmail.com>
2020-11-30 19:08:20 -0500

    remove unused code

--- src/utils/Playground/jwt.ts
+++ src/utils/Playground/jwt.ts
@@ -1,37 +1,10 @@
-import jwksClient, { JwksClient, CertSigningKey, RsaSigningKey } from "jwks-rsa"
+import jwksClient, { JwksClient } from "jwks-rsa"
 import jwt, { JwtHeader } from "jsonwebtoken"
 import { promisify } from "util"
 
 const client: JwksClient = jwksClient({
   jwksUri: process.env.GATSBY_JWKS_URI as string,
 })
-
-const getKey: jwt.GetPublicKeyOrSecret = function (header, callback) {
-  client.getSigningKey(header.kid as string, (err, key) => {
-    const signingKey =
-      (key as CertSigningKey).publicKey || (key as RsaSigningKey).rsaPublicKey
-    callback(null, signingKey)
-  })
-}
-
-type Callback = (...args: any[]) => void
-type MakeCbByName = (name?: string, cb?: Callback) => jwt.VerifyCallback
-const makeCbByName: MakeCbByName = (name, cb) =>
-  function (err, decoded) {
-    console.group(name ?? "unknown")
-    if (err) {
-      console.error("err", err)
-      cb?.(err)
-    } else if (decoded) {
-      console.log("decoded", decoded)
-      cb?.(decoded)
-    }
-    console.groupEnd()
-  }
-
-export const verifyToken = (token: string, cb?: Callback) => {
-  jwt.verify(token, getKey, makeCbByName("VERIFY:: ACCESS TOKEN", cb))
-}
 
 /**
  * A promisified version of `getKey` from the jsonwebtoken docs
