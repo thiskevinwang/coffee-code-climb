1b62ccf73acecadb422b101573d33878ce9e5f2a
Kevin Wang <kwangsan@gmail.com>
2020-12-06 18:11:43 -0500

    avatar url cache management via IDs

--- src/components/App/Settings.tsx
+++ src/components/App/Settings.tsx
@@ -95,7 +95,7 @@ const useStyles = makeStyles((theme) => {
 })
 
 const UPDATE_USERNAME = gql`
-  mutation UpdateUsername($id: String!, $username: String!) {
+  mutation UpdateUsername($id: ID!, $username: String!) {
     updateUsername(id: $id, username: $username) {
       id
       PK
